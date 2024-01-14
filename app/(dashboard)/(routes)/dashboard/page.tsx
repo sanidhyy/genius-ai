1a09564747fc51caf8dc2c3f80e35c3adb8242fa
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-14 09:31:58 +0000

    dashboard page finished

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,17 @@
-"use client"
+"use client";
 
-import { MessageSquare } from "lucide-react";
+import {
+  ArrowRight,
+  Code,
+  ImageIcon,
+  MessageSquare,
+  Music,
+  VideoIcon,
+} from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 
 const tools = [
   {
@@ -8,11 +19,41 @@ const tools = [
     icon: MessageSquare,
     color: "text-violet-500",
     bgColor: "bg-violet-500/10",
-    href: "/conversation"
-  }
-]
+    href: "/conversation",
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-violet-500/10",
+    href: "/music",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: "/image",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+    href: "/video",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
+    href: "/code",
+  },
+];
 
 const DashboardPage = () => {
+  const router = useRouter();
+
   return (
     <div>
       <div className="mb-8 space-y-4">
@@ -25,9 +66,23 @@ const DashboardPage = () => {
       </div>
 
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
-      {tools.map((tool) => (
-        
-      ))}
+        {tools.map((tool) => (
+          <Card
+            key={tool.href}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+            onClick={() => router.push(tool.href)}
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)} />
+              </div>
+
+              <div className="font-semibold">{tool.label}</div>
+            </div>
+
+            <ArrowRight className="w-5 h-5" />
+          </Card>
+        ))}
       </div>
     </div>
   );
