fc327af6e18c3d3376a1303020edf1b2ce70f373
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-14 10:37:14 +0000

    env types added

---
+++ environment.d.ts
@@ -0,0 +1,11 @@
+// This file is needed to support autocomplete for process.env
+export {};
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      // openai api key
+      OPENAI_API_KEY: string;
+    }
+  }
+}
