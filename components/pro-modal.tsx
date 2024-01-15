ff62acaea0b4c02dc558c4e5b212b8df2e70dd19
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-15 14:11:19 +0000

    stripe checkout added

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import axios from "axios";
 import { Check, Zap } from "lucide-react";
+import { useState } from "react";
 
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
@@ -17,9 +21,24 @@ import { cn } from "@/lib/utils";
 
 export const ProModal = () => {
   const proModal = useProModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await axios.get("/api/stripe");
+
+      window.location.href = response.data.url;
+    } catch (error: unknown) {
+      console.error("[STRIPE_CLIENT_ERROR]: ", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
-    <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
+    <Dialog open={isLoading || proModal.isOpen} onOpenChange={proModal.onClose}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
@@ -52,7 +71,14 @@ export const ProModal = () => {
         </DialogHeader>
 
         <DialogFooter>
-          <Button size="lg" variant="premium" className="w-full">
+          <Button
+            size="lg"
+            variant="premium"
+            className="w-full"
+            onClick={onSubscribe}
+            disabled={isLoading}
+            aria-disabled={isLoading}
+          >
             Upgrade
             <Zap className="w-4 h-4 ml-4 fill-white" />
           </Button>
