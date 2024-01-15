fe70fbc6a22e1ba67483ee0d6be2b30b1a031e1e
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-15 11:49:37 +0000

    api limit db implemented

---
+++ lib/api-limit.ts
@@ -0,0 +1,46 @@
+import { auth } from "@clerk/nextjs";
+
+import { MAX_FREE_COUNTS } from "@/config";
+
+import { db } from "./db";
+
+export const increaseApiLimit = async () => {
+  const { userId } = auth();
+
+  if (!userId) return;
+
+  const userApiLimit = await db.userApiLimit.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  if (userApiLimit) {
+    await db.userApiLimit.update({
+      where: {
+        userId,
+      },
+      data: {
+        count: userApiLimit.count + 1,
+      },
+    });
+  } else {
+    await db.userApiLimit.create({
+      data: { userId, count: 1 },
+    });
+  }
+};
+
+export const checkApiLimit = async () => {
+  const { userId } = auth();
+
+  if (!userId) return false;
+
+  const userApiLimit = await db.userApiLimit.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  return !userApiLimit || userApiLimit.count < MAX_FREE_COUNTS;
+};
