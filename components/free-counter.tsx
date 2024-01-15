83c6ff1f485b6fb5f0cc8ee7e2a8b337dc8d34b1
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-15 12:17:53 +0000

    free counter component completed

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { Zap } from "lucide-react";
 import { useEffect, useState } from "react";
 
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
 import { MAX_FREE_COUNTS } from "@/config";
 
 type FreeCounterProps = {
@@ -26,7 +29,17 @@ export const FreeCounter = ({ apiLimitCount = 0 }: FreeCounterProps) => {
             <p>
               {apiLimitCount} / {MAX_FREE_COUNTS} Free Generations
             </p>
+
+            <Progress
+              className="h-3 text-white"
+              value={(apiLimitCount / MAX_FREE_COUNTS) * 100}
+              indicatorColor="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+            />
           </div>
+
+          <Button variant="premium" className="w-full">
+            Upgrade <Zap className="w-4 h-4 ml-2 fill-white" />
+          </Button>
         </CardContent>
       </Card>
     </div>
