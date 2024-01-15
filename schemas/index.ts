853c917fd238bd0feff6d4db8b676fb5a619eece
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-15 10:05:25 +0000

    code and image schema created

--- schemas/index.ts
+++ schemas/index.ts
@@ -5,3 +5,17 @@ export const conversationFormSchema = z.object({
     message: "Prompt is required.",
   }),
 });
+
+export const codeFormSchema = z.object({
+  prompt: z.string().min(1, {
+    message: "Prompt is required.",
+  }),
+});
+
+export const imageFormSchema = z.object({
+  prompt: z.string().min(1, {
+    message: "Image Prompt is required.",
+  }),
+  amount: z.string().min(1),
+  resolution: z.string().min(1),
+});
