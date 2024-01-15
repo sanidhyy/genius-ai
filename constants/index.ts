9962f1bfc880bf1182aa409863ef6d0cbb11de8d
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-15 12:44:15 +0000

    tools and sidebar routes data added

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,65 @@
-export const MAX_FREE_COUNTS = 5;
+import {
+  Code,
+  ImageIcon,
+  LayoutDashboard,
+  MessageSquare,
+  Music,
+  Settings,
+  VideoIcon,
+} from "lucide-react";
+
+export const MAX_FREE_COUNTS = 5 as const;
+
+export const TOOLS = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-violet-500/10",
+    href: "/music",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: "/image",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+    href: "/video",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
+    href: "/code",
+  },
+] as const;
+
+export const ROUTES = [
+  {
+    label: "Dashboard",
+    icon: LayoutDashboard,
+    href: "/dashboard",
+    color: "text-sky-500",
+  },
+  ...TOOLS,
+  {
+    label: "Settings",
+    icon: Settings,
+    href: "/settings",
+    color: null,
+  },
+] as const;
