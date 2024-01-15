1ee8b6bdc8c6c2c72a87e72b30584e68a7f8997a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-15 12:44:26 +0000

    fix z index

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -9,7 +9,7 @@ const DashboardLayout = async ({ children }: PropsWithChildren) => {
 
   return (
     <div className="h-full relative">
-      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-900">
         <Sidebar apiLimitCount={apiLimitCount} />
       </div>
 
