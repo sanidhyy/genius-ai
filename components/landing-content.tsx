bcf8bd15ad3d466c0eba7b5a85ea89d9831f2a5f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-15 16:56:20 +0000

    landing page completed

---
+++ components/landing-content.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { TESTIMONIALS } from "@/constants";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export const LandingContent = () => {
+  return <div className="px-10 pb-20">
+    <h2 className="text-center text-4xl text-white font-extrabold mb-10">Testimonials</h2>
+
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {TESTIMONIALS.map((testimonial) => (
+            <Card key={testimonial.description} className="bg-[#192339] border-none text-white">
+                <CardHeader>
+                    <CardTitle className="flex items-center gap-x-2">
+                        <div>
+                            <p className="text-lg">{testimonial.name}</p>
+                            <p className="text-zinc-400 text-sm">{testimonial.title}</p>
+                        </div>
+                    </CardTitle>
+
+                    <CardContent className="pt-4 px-0">
+                        {testimonial.description}
+                    </CardContent>
+                </CardHeader>
+            </Card>
+        ))}
+    </div>
+  </div>;
+};
