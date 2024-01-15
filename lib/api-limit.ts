0c8fbe31ddbe3260ab8c08855f0518b13a745304
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-15 12:06:17 +0000

    api limit count util added

--- lib/api-limit.ts
+++ lib/api-limit.ts
@@ -44,3 +44,19 @@ export const checkApiLimit = async () => {
 
   return !userApiLimit || userApiLimit.count < MAX_FREE_COUNTS;
 };
+
+export const getApiLimitCount = async () => {
+  const { userId } = auth();
+
+  if (!userId) return 0;
+
+  const userApiLimit = await db.userApiLimit.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  if (!userApiLimit) return 0;
+
+  return userApiLimit.count;
+};
