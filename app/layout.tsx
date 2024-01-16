f63ffd020a9985a7cb1e4e2c67bcd7f07c20f27a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-16 07:46:37 +0000

    site metadata updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import type { PropsWithChildren } from "react";
 
+import { siteConfig } from "@/config";
 import { CrispProvider } from "@/providers/crisp-provider";
 import { ModalProvider } from "@/providers/modal-provider";
 import { ToasterProvider } from "@/providers/toaster-provider";
@@ -11,10 +12,7 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Genius",
-  description: "A Modern Next.js 14 SaaS AI Platform.",
-};
+export const metadata: Metadata = siteConfig;
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
---
+++ config/index.ts
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "Genius",
+  description: "A Modern Next.js 14 SaaS AI Platform.",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "netlify",
+    "react",
+    "genius-ai",
+    "artifical-intelligence",
+    "shadcn",
+    "shadcn-ui",
+    "radix-ui",
+    "cn",
+    "clsx",
+    "saas",
+    "aiven",
+    "sonner",
+    "zustand",
+    "zod",
+    "mysql",
+    "sql",
+    "lucide-react",
+    "crisp-chat",
+    "postcss",
+    "prettier",
+    "react-dom",
+    "tailwindcss",
+    "tailwindcss-animate",
+    "ui/ux",
+    "js",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "Sanidhya Kumar Verma",
+    url: "https://github.com/sanidhyy",
+  },
+} as const;
+
+export const links = {
+  sourceCode: "https://github.com/sanidhyy/genius-ai",
+} as const;
