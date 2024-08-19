069803c24457da46593d155fd5ca2f1a3380cce3
Leo Espinoza <leo@holion.ai>
2024-08-19 13:57:33 -0500

    actualizar

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -19,9 +19,9 @@ export const LandingHero = () => {
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
         <h1>{CAPABILITIES_TITLE}</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-		  <TypewriterComponent
+        <TypewriterComponent
             options={{
-              strings: CAPABILITIES_TOOLS, // Use the strings constant
+              strings: [...CAPABILITIES_TOOLS], // Use the strings constant
               autoStart: true,
               loop: true,
             }}
