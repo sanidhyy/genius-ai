837baf3f64a1fd6e8a37adef5c2585e8a51a300f
Sanidhya Kumar Verma <sanidhyyy@gmail.com>
2026-09-01 15:13:26 +0530

    chore: removed old chatgpt domain from dall-e-2

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "oaidalleapiprodscus.blob.core.windows.net",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
